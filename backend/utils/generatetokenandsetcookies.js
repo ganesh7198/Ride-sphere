fc42611dc049b67cd0fb3c6d993346a61b2de48e
ganesh7198 <gg7198596@gmail.com>
2026-04-01 18:36:07 +0530

    h

--- backend/utils/generatetokenandsetcookies.js
+++ backend/utils/generatetokenandsetcookies.js
@@ -7,12 +7,12 @@ export const generateTokenAndSetCookie = (userId, res) => {
     { expiresIn: "1d" }           
   );
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 24 * 60 * 60 * 1000, 
-  });
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,          
+  sameSite: "none",      
+  maxAge: 24 * 60 * 60 * 1000,
+});
 
   return token;
 };
